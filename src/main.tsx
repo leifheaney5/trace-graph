68dfc5150794e117becedd32d578f4b9ad5ae4c6
Leif Heaney <127333147+leifheaney5@users.noreply.github.com>
2026-09-04 15:31:04 -0400

    feat: mount case study workflow chrome

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./case-study-chrome.css";
 import App from "./App.tsx";
+import CaseStudyChrome from "./CaseStudyChrome.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <CaseStudyChrome />
     <App />
   </StrictMode>,
 );
